0a8e83fba152cd1e6fffe69d1b1799d2fcc9016a
1998MaxChykalo <maxymchykalo@gmail.com>
2018-08-05 14:29:49 +0300

    Add logging and error handling

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
@@ -14,13 +17,26 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use('', (req, res, next) => {
-    console.log(req);
-    next();
-});
-
+// add logging 
+app.use(morgan('dev'));
   
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// if we didn't find a fitting route, return not found
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
