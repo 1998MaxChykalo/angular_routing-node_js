94b202bfd5d837e3770b29a5abc1e01254af1583
1998MaxChykalo <maxymchykalo@gmail.com>
2018-08-05 15:02:56 +0300

    Update CORS

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -10,10 +10,16 @@ const orderRoutes = require('./api/routes/orders');
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
+app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+// add CORS
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
     next();
 });
 
